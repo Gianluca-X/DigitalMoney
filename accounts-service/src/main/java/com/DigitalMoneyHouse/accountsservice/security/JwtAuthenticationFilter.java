97ea8aed13dc6d3c1733e1b3ed1805a973d6752e
Gianluca Fucci <100246766+Gianluca-X@users.noreply.github.com>
2026-02-17 18:57:28 +0000

    arreglo

--- accounts-service/src/main/java/com/DigitalMoneyHouse/accountsservice/security/JwtAuthenticationFilter.java
+++ accounts-service/src/main/java/com/DigitalMoneyHouse/accountsservice/security/JwtAuthenticationFilter.java
@@ -8,7 +8,6 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -21,9 +20,10 @@ import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
-@Slf4j
 @Component
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    private static final Logger log =
+        LoggerFactory.getLogger(JwtAuthenticationFilter.class);
 
     @Value("${jwt.secret}")
     private String secretKey;
