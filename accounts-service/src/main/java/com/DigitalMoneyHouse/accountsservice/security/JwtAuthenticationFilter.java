7c1685f96b239256158d93ea8f6caef29b0909c2
Gianluca Fucci <100246766+Gianluca-X@users.noreply.github.com>
2026-02-17 19:01:28 +0000

    arreglo

--- accounts-service/src/main/java/com/DigitalMoneyHouse/accountsservice/security/JwtAuthenticationFilter.java
+++ accounts-service/src/main/java/com/DigitalMoneyHouse/accountsservice/security/JwtAuthenticationFilter.java
@@ -16,6 +16,9 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
