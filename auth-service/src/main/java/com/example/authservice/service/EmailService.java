5057992a364c9d1afc44516e6c50e09ce653a806
Gianluca Fucci <100246766+Gianluca-X@users.noreply.github.com>
2026-02-21 02:42:25 +0000

    FIX SENDER

--- auth-service/src/main/java/com/example/authservice/service/EmailService.java
+++ auth-service/src/main/java/com/example/authservice/service/EmailService.java
@@ -1,28 +1,42 @@
 package com.example.authservice.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import com.sendgrid.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
 
 @Service
 public class EmailService {
-    @Autowired
-    private JavaMailSender mailSender;
 
-    public void sendEmail(String to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
+    @Value("${sendgrid.api-key}")
+    private String apiKey;
+
+    @Value("${sendgrid.from}")
+    private String fromEmail;
+
+    public void sendEmail(String to, String subject, String body) throws IOException {
+
+        Email from = new Email(fromEmail);
+        Email recipient = new Email(to);
+        Content content = new Content("text/plain", body);
+
+        Mail mail = new Mail(from, subject, recipient, content);
+
+        SendGrid sg = new SendGrid(apiKey);
+        Request request = new Request();
+
+        request.setMethod(Method.POST);
+        request.setEndpoint("mail/send");
+        request.setBody(mail.build());
+
+        sg.api(request);
     }
 
-    public void sendVerificationEmail(String email, String verificationCode, String verificationLink) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(email);
-        message.setSubject("Verify your email for Digital Money House");
-        message.setText("Your verification code is: " + verificationCode + " Click to verify your email: " + verificationLink);
-        mailSender.send(message);
+    public void sendVerificationEmail(String email, String verificationCode, String verificationLink) throws IOException {
+        String body = "Your verification code is: " + verificationCode +
+                "\nClick to verify your email: " + verificationLink;
+
+        sendEmail(email, "Verify your email for Digital Money House", body);
     }
 }
